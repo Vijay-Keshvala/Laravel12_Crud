2c6bbd5f27e4a25c62a14ca49ee2b27a5f52ce86
Vijay-Keshvala <keshvalavijay11@gmail.com>
2025-07-18 17:40:14 +0530

    bug fix

--- routes/web.php
+++ routes/web.php
@@ -11,5 +11,6 @@ Route::get('/', function () {
 Route::get('/products', [ProductController::class, 'index'])->name('products.index');
 Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
 Route::post('/products', [ProductController::class, 'store'])->name('products.store');
+Route::put('/products/{product}', [ProductController::class, 'update'])->name('products.update');
 Route::get('/products/{product}/edit', [ProductController::class, 'edit'])->name('products.edit');
 Route::delete('/products/{product}', [ProductController::class, 'destroy'])->name('products.destroy');
